17c21d4313764891acb03c0310aaad4c2f2508ec
winwithcherwin <winwithcherwin@gmail.com>
2025-03-22 17:56:46 +0100

    Migrate to venv

--- oblivion/engine/ansible/tasks.py
+++ oblivion/engine/ansible/tasks.py
@@ -1,16 +1,28 @@
 import ansible_runner
+import os
 
 from celery import shared_task
 
 
 @shared_task
 def run_playbook_locally(playbook_path: str):
+    private_data_dir = "/tmp/ansible-run"
+    os.makedirs(private_data_dir, exist_ok=True)
+
     runner = ansible_runner.run(
-        private_data_dir="/tmp/ansible-run",
+        private_data_dir=private_data_dir,
         playbook=playbook_path,
-        inventory="localhost,",
+        inventory={
+            "all": {
+                "hosts": {
+                    "localhost": {
+                        "ansible_connection": "local"
+                    }
+                  }
+                }
+              },
         limit="localhost",
-        quiet=True
+        quiet=True,
     )
 
     return {
--- terraform/modules/oblivion/templates/oblivion.service.tpl
+++ terraform/modules/oblivion/templates/oblivion.service.tpl
@@ -1,6 +1,7 @@
 [Unit]
 Description=Oblivion Worker
 After=network.target
+ConditionPathExists=/opt/oblivion/oblivion/celery_app.py
 
 [Service]
 Type=simple
@@ -8,9 +9,9 @@ User=root
 Group=root
 WorkingDirectory=/opt/oblivion
 EnvironmentFile=/etc/oblivion.env
-Environment="PYTHONPATH=/opt/oblivion"
 Environment="C_FORCE_ROOT=1"
-ExecStart=/usr/bin/python3 -m celery -A oblivion.celery_app worker --loglevel=debug --queues=${queue_name}
+Environment="PATH=/opt/oblivion-venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin"
+ExecStart=/opt/oblivion-venv/bin/celery -A oblivion.celery_app worker --loglevel=debug --queues=${queue_name}
 Restart=always
 
 [Install]
