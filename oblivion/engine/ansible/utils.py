113ecfab340f8315cdb8b294aa709d8595a264b0
winwithcherwin <winwithcherwin@gmail.com>
2025-03-28 19:56:35 +0100

    Print out just the name when listing site playbooks

--- oblivion/engine/ansible/utils.py
+++ oblivion/engine/ansible/utils.py
@@ -17,6 +17,8 @@ def list_available_playbooks():
                 full_path = os.path.join(dirpath, f)
                 rel_path = os.path.relpath(full_path, PLAYBOOK_ROOT)
                 clean_path = os.path.splitext(rel_path)[0]  # strip .yaml
+                if clean_path.endswith("site"):
+                    clean_path = clean_path.split("/")[0]
                 playbooks.append(clean_path)
     return sorted(playbooks)
 
