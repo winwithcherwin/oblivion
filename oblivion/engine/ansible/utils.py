92926d741c44b1d9a7dc5d262131aee5283b0f44
winwithcherwin <winwithcherwin@gmail.com>
2025-03-28 19:52:42 +0100

    Cap dirpath 2 levels deepp when listing playbooks

--- oblivion/engine/ansible/utils.py
+++ oblivion/engine/ansible/utils.py
@@ -4,10 +4,17 @@ PLAYBOOK_ROOT = os.path.abspath("oblivion/engine/ansible/playbooks")
 
 def list_available_playbooks():
     playbooks = []
-    for root, _, files in os.walk(PLAYBOOK_ROOT):
+    for dirpath, dirnames, files in os.walk(PLAYBOOK_ROOT):
+        rel_path = os.path.relpath(dirpath, PLAYBOOK_ROOT)
+        depth = rel_path.count(os.sep)
+
+        if depth >= 2:
+            dirnames[:] = []
+            continue
+
         for f in files:
             if f.endswith((".yaml", ".yml")):
-                full_path = os.path.join(root, f)
+                full_path = os.path.join(dirpath, f)
                 rel_path = os.path.relpath(full_path, PLAYBOOK_ROOT)
                 clean_path = os.path.splitext(rel_path)[0]  # strip .yaml
                 playbooks.append(clean_path)
